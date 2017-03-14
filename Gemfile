e2b0f226f624e932936d2699b6809cdb426d3ce9
jasonzou <jason.zou@gmail.com>
2017-03-14 13:31:04 -0400

    learning devise ...

--- Gemfile
+++ Gemfile
@@ -61,3 +61,4 @@ gem "twitter-bootstrap-rails"
 
 gem 'twitter-bootswatch-rails'
 gem 'twitter-bootswatch-rails-helpers'
+gem 'devise'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
-  has_many :microposts
-  validates :name, presence: true
-  validates :email, presence: true
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  #has_many :microposts
+  #validates :name, presence: true
+  #validates :email, presence: true
 end
